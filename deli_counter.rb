0e0a2901cd7e4ab5c4fd1692eab2a16aed700d80
John Exarhouleas <exarhouleasjohn@gmail.com>
2020-08-27 18:01:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,12 +3,20 @@ katz_deli = []
 def line(array)
   i = 1
   if array.length != 0  
+<<<<<<< HEAD
      string = "The line is currently:"
      array.each do |index|
      string = "#{string} #{i}. #{index}"
       i += 1
      end
      puts string
+=======
+     print "The line is currently:"
+     array.each do |index|
+      print " #{i}. #{index}"
+      i += 1
+     end
+>>>>>>> 4496d9d10f220b28f3d8384bbce487dd78fd7cb0
   else 
     puts "The line is currently empty."
   end
